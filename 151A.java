4e83797721b78a0816b17b4ed53f1852e74d7dd1
yash <notaxxid@yashmac.local>
2026-07-21 15:13:22 +0530

    151A.java

---
+++ 151A.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int l = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+        int p = sc.nextInt();
+        int nl = sc.nextInt();
+        int np = sc.nextInt();
+
+        int drink = (k * l) / nl;
+        int lime = c * d;
+        int salt = p / np;
+
+        int ans = Math.min(drink, Math.min(lime, salt)) / n;
+
+        System.out.println(ans);
+    }
+}
