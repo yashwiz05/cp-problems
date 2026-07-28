a070c32e018ceabd9abc159239cfbaef0000c9b3
yash <notaxxid@yashmac.local>
2026-07-28 22:40:54 +0530

    1030A.java

---
+++ 1030A.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        boolean hard = false;
+
+        for (int i = 0; i < n; i++) {
+            int x = sc.nextInt();
+
+            if (x == 1)
+                hard = true;
+        }
+
+        if (hard)
+            System.out.println("HARD");
+        else
+            System.out.println("EASY");
+    }
+}
