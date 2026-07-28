3ec132e2e97b4b3a4506523f1c70360de672b295
yash <notaxxid@yashmac.local>
2026-07-28 22:19:58 +0530

    1749A.java

---
+++ 1749A.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        while (t-- > 0) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+
+            if (a + b == c || a + c == b || b + c == a)
+                System.out.println("YES");
+            else
+                System.out.println("NO");
+        }
+    }
+}
